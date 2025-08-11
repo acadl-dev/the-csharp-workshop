20702919d931b4603d410828609241b4f62d34aa
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2025-08-11 07:58:51 -0300

    Exercises 2 and 3

---
+++ Exercises/Chapter01/Exercise1_02/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Hi! i'm your first C# Program. What's your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello {name}, it is very nice to meet you. We have a really fun journey ahead.");
---
+++ Exercises/Chapter01/Exercise1_03/Program.cs
@@ -0,0 +1,23 @@
+﻿// This method attempts to modify the received text,
+// but since strings are immutable in C#, the change will not affect the original variable.
+// The result of Replace is not stored or returned.
+
+static void FormatString(string stringToFormat)
+{
+    stringToFormat.Replace("World", "Mars");  /* Here it returns me a modified string */
+}
+
+
+// This method returns a new string with "Earth" replaced by "Mars".
+// Because strings are immutable, the modified value must be returned.
+static string FormatReturningString(string stringToFormat)
+{
+    return stringToFormat.Replace("Earth", "Mars");
+}
+
+var greetings = "Hello World";
+FormatString(greetings);
+Console.WriteLine(greetings); // This will print "Hello World" because FormatString does not modify the original string.
+
+var anotherGreetings = "Good morning Earth!";
+Console.WriteLine(FormatReturningString(anotherGreetings)); // This will print "Hello Mars" because the method returns a new string with the modification.
